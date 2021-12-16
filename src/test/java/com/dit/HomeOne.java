3979a0972a2edcebed059a35ff33ef05cbf7c735
Andre0023 <Vash0023@gmail.com>
2021-12-16 17:21:52 +0300

    Подготовка к тесту

---
+++ src/test/java/com/dit/HomeOne.java
@@ -0,0 +1,35 @@
+package com.dit;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+    @DisplayName("Тест формы")
+public class HomeOne {
+        @Test
+        void hOne() {
+            open("https://demoqa.com/automation-practice-form");
+            //firstName
+
+            //lastName
+
+            //userEmail
+
+            //gender-radio-1
+
+            //userNumber
+
+            //dateOfBirthInput
+
+            //hobbies-checkbox-1
+
+            //uploadPicture
+
+            //currentAddress
+
+            //currentAddress
+
+
+        }
+    }
+
