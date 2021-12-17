279dc2393b3a0ae007726d821ec1bbf237011770
Andre0023 <Vash0023@gmail.com>
2021-12-17 17:34:16 +0300

    Завершение ДЗ

--- src/test/java/com/dit/HomeOne.java
+++ src/test/java/com/dit/HomeOne.java
@@ -1,22 +1,21 @@
 package com.dit;
+import org.junit.jupiter.api.AfterEach;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
-import java.io.File;
-
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byId;
+import java.io.File;
+import com.codeborne.selenide.CollectionCondition;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selectors.byText;
 
 public class HomeOne {
         @BeforeAll
@@ -24,6 +23,7 @@ public class HomeOne {
             Configuration.browserSize = "1920x1080";
         }
         @Test
+        @DisplayName("ввод")
         void hOne() {
             open("https://demoqa.com/automation-practice-form");
 
@@ -37,8 +37,12 @@ public class HomeOne {
             //выбор пола
             $("#userNumber").setValue("99999999999");
             //номер телефона
-            $("#dateOfBirthInput").setValue("16 Dec 1999");
-            $("[for='gender-radio-1']").click();
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").click();
+            $("[value='10']").click();
+            $(".react-datepicker__year-select").click();
+            $("[value='1992']").click();
+            $(".react-datepicker__day--002").click();
             //Дата рождения
             $("#subjectsInput").setValue("C");
             $(Selectors.byText("Computer Science")).scrollTo().click();
@@ -47,36 +51,41 @@ public class HomeOne {
             //хобби
             $("input#uploadPicture").uploadFile(new File("C:\\Users\\64an\\Pictures\\MY.jpg"));
             //загрузка фото
-
             $("#currentAddress").setValue("New Sol");
             //андрес проживания
-
-            //$("#lastName").setValue();
+            $(Selectors.byText("Select State")).click();
+            $(Selectors.byText("Haryana")).click();
+            $(Selectors.byText("Select City")).click();
+            $(Selectors.byText("Karnal")).click();
             //Всплывающий список с городом
 
             $("#submit").click();
             //кнопка отправки
-        }
-        @Test
-        void prow() {
-            $("example-modal-sizes-title-lg").shouldBe(Condition.visible);
-           // $("").shouldHave(text());//имя
-           // $("").shouldHave(text());//фамилия
-           // $("").shouldHave(text());//почта
-           // $("").shouldHave(text());//выбор пола
-          //  $("").shouldHave(text());//номер телефона
-          //  $("").shouldHave(text());//Дата рождения
-          //  $("").shouldHave(text());//хобби
-          //  $("").shouldHave(text());//загрузка фото
-          //  $("").shouldHave(text());//андрес проживания
-           // $("").shouldHave(text());//Всплывающий список с городом
 
 
+            //$("#closeLargeModal").click();
 
 
         }
-
-
-
+    @DisplayName("Проверка")
+    void checkResult() {
+        $(byText("Thanks for submitting the form")).should(appear);
+        $(byText("Andrei Ivanov")).should(appear);
+        $(byText("mail@mail.com")).should(appear);
+        $(byText("Male")).should(appear);
+        $(byText("99999999999")).should(appear);
+        $(byText("02 November,1992")).should(appear);
+        $(byText("Computer Science")).should(appear);
+        $(byText("Reading")).should(appear);
+        $(byText("MY.jpg")).should(appear);
+        $(byText("Saint-Pee")).should(appear);
+        $(byText("Haryana Karnal")).should(appear);
     }
+    @AfterEach
+    void after(){
+        closeWebDriver();
+    }
+
+}
+
 
