303085d88e6a488d3a7b9da50be72e7005289711
Andre0023 <Vash0023@gmail.com>
2021-12-23 20:01:26 +0300

    Завершение ДЗ1,1

--- src/test/java/com/dit/HomeOne.java
+++ src/test/java/com/dit/HomeOne.java
@@ -1,4 +1,5 @@
 package com.dit;
+
 import org.junit.jupiter.api.AfterEach;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
@@ -9,64 +10,66 @@ import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.visible;
+
 import java.io.File;
+
 import com.codeborne.selenide.CollectionCondition;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selectors.byText;
 
 public class HomeOne {
-        @BeforeAll
-        static void beforeAll(){
-            Configuration.browserSize = "1920x1080";
-        }
-        @Test
-        @DisplayName("ввод")
-        void hOne() {
-            open("https://demoqa.com/automation-practice-form");
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
 
-            $("#firstName").setValue("Andrei");
-            //имя
-            $("#lastName").setValue("Ivanov");
-            //фамилия
-            $("#userEmail").setValue("mail@mail.com");
-            //почта
-            $("[for='gender-radio-1']").click();
-            //выбор пола
-            $("#userNumber").setValue("99999999999");
-            //номер телефона
-            $("#dateOfBirthInput").click();
-            $(".react-datepicker__month-select").click();
-            $("[value='10']").click();
-            $(".react-datepicker__year-select").click();
-            $("[value='1992']").click();
-            $(".react-datepicker__day--002").click();
-            //Дата рождения
-            $("#subjectsInput").setValue("C");
-            $(Selectors.byText("Computer Science")).scrollTo().click();
-            //Subjects
-            $("[for='hobbies-checkbox-2']").click();
-            //хобби
-            $("input#uploadPicture").uploadFile(new File("C:\\Users\\64an\\Pictures\\MY.jpg"));
-            //загрузка фото
-            $("#currentAddress").setValue("New Sol");
-            //андрес проживания
-            $(Selectors.byText("Select State")).click();
-            $(Selectors.byText("Haryana")).click();
-            $(Selectors.byText("Select City")).click();
-            $(Selectors.byText("Karnal")).click();
-            //Всплывающий список с городом
+    @Test
+    @DisplayName("ввод")
+    void hOne() {
+        open("https://demoqa.com/automation-practice-form");
 
-            $("#submit").click();
-            //кнопка отправки
+        $("#firstName").setValue("Andrei"); //имя
+
+        $("#lastName").setValue("Ivanov"); //фамилия
+
+        $("#userEmail").setValue("mail@mail.com"); //почта
+
+        $("[for='gender-radio-1']").click(); //выбор пола
+
+        $("#userNumber").setValue("99999999999"); //номер телефона
+
+        $("#dateOfBirthInput").click(); //Дата рождения
+        $(".react-datepicker__month-select").click();
+        $("[value='10']").click();
+        $(".react-datepicker__year-select").click();
+        $("[value='1992']").click();
+        $(".react-datepicker__day--002").click();
+
+        $("#subjectsInput").setValue("C");
+        $(Selectors.byText("Computer Science")).scrollTo().click(); //Subjects
+
+        $("[for='hobbies-checkbox-2']").click(); //хобби
+
+        $("#uploadPicture").uploadFile(new File("C:\\Users\\64an\\Pictures\\MY.jpg")); //загрузка фото
+
+        $("#currentAddress").setValue("New Sol"); //андрес проживания
+
+        $(Selectors.byText("Select State")).click(); //Всплывающий список с городом
+        $(Selectors.byText("Haryana")).click();
+        $(Selectors.byText("Select City")).click();
+        $(Selectors.byText("Karnal")).click();
 
 
-            //$("#closeLargeModal").click();
+        $("#submit").click(); //кнопка отправки
 
 
-        }
+    }
+
     @DisplayName("Проверка")
     void checkResult() {
         $(byText("Thanks for submitting the form")).should(appear);
@@ -81,8 +84,9 @@ public class HomeOne {
         $(byText("Saint-Pee")).should(appear);
         $(byText("Haryana Karnal")).should(appear);
     }
+
     @AfterEach
-    void after(){
+    void after() {
         closeWebDriver();
     }
 
